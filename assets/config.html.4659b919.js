a6797bd7759f79fa5293cd8a117f3dc1944b1075
Reonra <49245772+reonra@users.noreply.github.com>
2023-01-07 00:11:31 +0800

    Add files via upload

---
+++ assets/config.html.4659b919.js
@@ -0,0 +1,63 @@
+import{_ as e,o as s,c as i,a}from"./app.76f10a1d.js";const n={},l=a(`<h2 id="\u914D\u7F6E" tabindex="-1"><a class="header-anchor" href="#\u914D\u7F6E" aria-hidden="true">#</a> \u914D\u7F6E</h2><h3 id="\u666E\u901A\u6807\u7B7E" tabindex="-1"><a class="header-anchor" href="#\u666E\u901A\u6807\u7B7E" aria-hidden="true">#</a> \u666E\u901A\u6807\u7B7E</h3><p><img src="https://svg.hamm.cn/badge.svg?key=\u82AC&amp;value=\u5FC5\u5F97&amp;color=ff4500&amp;radius=3" alt="SVG" title="SVG"> <img src="https://svg.hamm.cn/badge.svg?key=\u535A\u5BA2&amp;value=Hamm.cn" alt="SVG" title="SVG"></p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>\u63A5\u5165\u5730\u5740\uFF1Ahttps://svg.hamm.cn/badge.svg
+\u53C2\u6570\u8BF4\u660E\uFF1A
+    key:\u6807\u7B7E\u5934 \u5FC5\u987B
+    value:\u6807\u7B7E\u5185\u5BB9 \u5FC5\u987B
+    radis:\u6807\u7B7E\u5706\u89D2 \u9009\u586B \u9ED8\u8BA43
+    color:\u6807\u7B7E\u989C\u8272 \u9009\u586B \u9ED8\u8BA4\u968F\u673A
+
+\u793A\u4F8B\uFF1A
+    https://svg.hamm.cn/badge.svg?key=\u82AC&amp;value=\u5FC5\u5F97&amp;color=ff4500&amp;radius=3
+    https://svg.hamm.cn/badge.svg?key=\u535A\u5BA2&amp;value=Hamm.cn
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="\u7801\u4E91\u7279\u6B8A\u6807\u7B7E" tabindex="-1"><a class="header-anchor" href="#\u7801\u4E91\u7279\u6B8A\u6807\u7B7E" aria-hidden="true">#</a> \u7801\u4E91\u7279\u6B8A\u6807\u7B7E</h3><ul><li>\u4ED3\u5E93\u6807\u7B7E</li></ul><p><img src="https://svg.hamm.cn/gitee.svg?user=hamm&amp;project=svg_badge_tool&amp;type=star" alt="SVG" title="SVG"> <img src="https://svg.hamm.cn/gitee.svg?user=hamm&amp;project=svg_badge_tool&amp;type=watch" alt="SVG" title="SVG"> <img src="https://svg.hamm.cn/gitee.svg?user=hamm&amp;project=svg_badge_tool&amp;type=fork" alt="SVG" title="SVG"> <img src="https://svg.hamm.cn/gitee.svg?user=hamm&amp;project=svg_badge_tool&amp;type=commit" alt="SVG" title="SVG"><img src="https://svg.hamm.cn/gitee.svg?user=hamm&amp;project=svg_badge_tool&amp;type=language" alt="SVG" title="SVG"> <img src="https://svg.hamm.cn/gitee.svg?user=hamm&amp;project=svg_badge_tool&amp;type=license" alt="SVG" title="SVG"> <img src="https://svg.hamm.cn/gitee.svg?user=hamm&amp;project=svg_badge_tool&amp;type=issue" alt="SVG" title="SVG"> <img src="https://svg.hamm.cn/gitee.svg?user=hamm&amp;project=svg_badge_tool&amp;type=branch" alt="SVG" title="SVG"> <img src="https://svg.hamm.cn/gitee.svg?user=hamm&amp;project=svg_badge_tool&amp;type=release" alt="SVG" title="SVG"></p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>\u63A5\u5165\u5730\u5740\uFF1Ahttps://svg.hamm.cn/gitee.svg
+\u53C2\u6570\u8BF4\u660E\uFF1A
+    user:\u7528\u6237 \u5FC5\u987B
+    project:\u9879\u76EE \u5FC5\u987B
+    type:\u7C7B\u578B \u5FC5\u987B \u56FA\u5B9A\u503C\u4E3A\uFF1A
+        - watch \u9879\u76EEwatch\u4EBA\u6570
+        - fork \u9879\u76EEfork\u4EBA\u6570
+        - star \u9879\u76EEstar\u6570\u91CF
+        - commit \u9879\u76EE\u63D0\u4EA4\u6B21\u6570
+        - license \u9879\u76EE\u4F7F\u7528\u7684\u5F00\u6E90\u534F\u8BAE
+        - language \u9879\u76EE\u4F7F\u7528\u4E3B\u8981\u7F16\u7A0B\u8BED\u8A00
+        - issue \u9879\u76EE\u6253\u5F00\u7684Issues\u6570\u91CF
+        - branch \u9ED8\u8BA4\u5206\u652F
+        - release \u6700\u65B0\u7684Release
+    radis:\u6807\u7B7E\u5706\u89D2 \u9009\u586B \u9ED8\u8BA43
+    color:\u6807\u7B7E\u989C\u8272 \u9009\u586B \u9ED8\u8BA4 #404550
+    bg:\u6807\u9898\u989C\u8272 \u9ED8\u8BA4Gitee\u4E3B\u9898\u8272 #c71d23
+    title:\u6807\u7B7E\u7684Title \u9009\u586B \u9ED8\u8BA4Gitee
+    logo:\u662F\u5426\u663E\u793AGitee Logo \u9ED8\u8BA4yes \u5176\u4ED6\u503C\u9690\u85CFLogo
+
+\u793A\u4F8B\uFF1A
+    https://svg.hamm.cn/gitee.svg?user=hamm&amp;project=svg_badge_tool&amp;type=star&amp;color=ff4500&amp;radius=3
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="\u7528\u6237\u6807\u7B7E" tabindex="-1"><a class="header-anchor" href="#\u7528\u6237\u6807\u7B7E" aria-hidden="true">#</a> \u7528\u6237\u6807\u7B7E</h3><p><img src="https://svg.hamm.cn/gitee-user.svg?user=hamm" alt="SVG" title="SVG"></p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>\u63A5\u5165\u5730\u5740\uFF1Ahttps://svg.hamm.cn/gitee-user.svg
+\u53C2\u6570\u8BF4\u660E\uFF1A
+    user:\u7528\u6237 \u5FC5\u987B
+
+\u793A\u4F8B\uFF1A
+    https://svg.hamm.cn/gitee-user.svg?user=hamm
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="\u5FAE\u535A\u7279\u6B8A\u6807\u7B7E" tabindex="-1"><a class="header-anchor" href="#\u5FAE\u535A\u7279\u6B8A\u6807\u7B7E" aria-hidden="true">#</a> \u5FAE\u535A\u7279\u6B8A\u6807\u7B7E</h3><p><img src="https://svg.hamm.cn/weibo.svg?user=5898441253&amp;type=followers" alt="SVG" title="SVG"> <img src="https://svg.hamm.cn/weibo.svg?user=5898441253&amp;type=following" alt="SVG" title="SVG"></p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>\u63A5\u5165\u5730\u5740\uFF1Ahttps://svg.hamm.cn/weibo.svg
+\u53C2\u6570\u8BF4\u660E\uFF1A
+    user:\u7528\u6237UID \u5FC5\u987B
+    type:\u7C7B\u578B \u5FC5\u987B \u56FA\u5B9A\u503C\u4E3A\uFF1A
+        - followers \u7C89\u4E1D\u4EBA\u6570
+        - following \u5173\u6CE8\u4EBA\u6570
+    radis:\u6807\u7B7E\u5706\u89D2 \u9009\u586B \u9ED8\u8BA43
+    color:\u6807\u7B7E\u989C\u8272 \u9009\u586B \u9ED8\u8BA4 #404550
+    bg:\u6807\u9898\u989C\u8272 \u9ED8\u8BA4Gitee\u4E3B\u9898\u8272 #c71d23
+    title:\u6807\u7B7E\u7684Title \u9009\u586B \u9ED8\u8BA4Gitee
+
+\u793A\u4F8B\uFF1A
+    https://svg.hamm.cn/weibo.svg?user=5898441253&amp;type=followers&amp;color=ff4500&amp;radius=3
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="\u5BA2\u6237\u7AEF\u7279\u6B8A\u6807\u7B7E" tabindex="-1"><a class="header-anchor" href="#\u5BA2\u6237\u7AEF\u7279\u6B8A\u6807\u7B7E" aria-hidden="true">#</a> \u5BA2\u6237\u7AEF\u7279\u6B8A\u6807\u7B7E</h3><p><img src="https://svg.hamm.cn/client.svg?type=os" alt="SVG" title="SVG"> <img src="https://svg.hamm.cn/client.svg?type=broswer" alt="SVG" title="SVG"> <img src="https://svg.hamm.cn/client.svg?type=ip" alt="SVG" title="SVG"></p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>\u63A5\u5165\u5730\u5740\uFF1Ahttps://svg.hamm.cn/client.svg
+\u53C2\u6570\u8BF4\u660E\uFF1A
+    type:\u7C7B\u578B \u5FC5\u987B \u56FA\u5B9A\u503C\u4E3A\uFF1A
+        - os \u64CD\u4F5C\u7CFB\u7EDF
+        - ip IP\u5730\u5740
+        - broswer \u6D4F\u89C8\u5668
+    radis:\u6807\u7B7E\u5706\u89D2 \u9009\u586B \u9ED8\u8BA43
+    color:\u6807\u7B7E\u989C\u8272 \u9009\u586B \u9ED8\u8BA4\u968F\u673A
+
+\u793A\u4F8B\uFF1A
+    https://svg.hamm.cn/client.svg?type=os&amp;color=ff4500&amp;radius=3
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,17),t=[l];function d(m,r){return s(),i("div",null,t)}const c=e(n,[["render",d],["__file","config.html.vue"]]);export{c as default};
